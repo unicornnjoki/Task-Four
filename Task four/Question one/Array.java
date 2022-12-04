e61855c23b8f399ea3cfcbdabb3fc5cbbaa92b19
unicornnjoki <118070879+unicornnjoki@users.noreply.github.com>
2022-12-04 22:29:27 +0300

    Add files via upload

---
+++ Task four/Question one/Array.java
@@ -0,0 +1,25 @@
+package array;
+
+public class Array {
+
+    
+    public static void main(String[] args) {
+      //declaration of variables
+      {
+      int i,a[15],num;
+        
+        for(i=0; i<15; i==)
+            printf("enter number :")
+                    {
+         if(num found)
+         printf("index of number")    
+                 
+                       {
+         if(num not found)
+         printf("not found in array ")    
+                      
+        }
+        }
+    }
+    }
+}
---
+++ Task four/Question two/Prime.java
@@ -0,0 +1,24 @@
+
+package prime;
+
+public class Prime {
+
+    
+    public static void main(String[] args) {
+      int i=0;
+      int num =0;
+      
+      for(i=1; i<=500;i++)
+      {
+          if(i%num==0)
+          {
+              counter=counter +1;
+          }
+      }
+      if (counter==2)
+      {
+          primenumbers=primenumbers+i"";
+      }
+    }
+    system.out.println("true:");
+          }
